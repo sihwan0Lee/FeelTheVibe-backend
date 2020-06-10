09b69a404f13e2a80b7b90c6a5e1e282b0274bee
misun9283 <kimmisun9283@naver.com>
2020-06-10 22:08:04 +0900

    [add] account, music models.py 작성

--- vibe_main/settings.py
+++ vibe_main/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
     'corsheaders',
+    'account',
+    'music',
 ]
 
 MIDDLEWARE = [
