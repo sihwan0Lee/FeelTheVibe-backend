d9d713e46b5f81bb368816ccc953008230fdf7ff
misun9283 <kimmisun9283@naver.com>
2020-06-11 13:49:50 +0900

    [add] models.py manytomanyfield 추가

--- music/views.py
+++ music/views.py
@@ -1,2 +1,14 @@
+import json
+
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse, JsonResponse
+
+from .models import Music, Album, Artist
+from account.models import User, MyPlaylist
+
+class MusicPlayView(View)
+
+    def get(self,request):
+
 
